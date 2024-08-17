8a1fbef6bc51eb114def2bea9f8302e6eff0a030
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-08-17 09:53:20 +0800

    ProductsAPIFetchSetUp

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,16 +1,32 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { DashboardMetricsType } from "./type";
+import { DashboardMetricsType, Product, NewProduct } from "./type";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: ["DashboardMetrics"],
+  tagTypes: ["DashboardMetrics","Products"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetricsType, void>({
       query: () => "/dashboard/metrics",
       providesTags: ["DashboardMetrics"],
     }),
+    getProducts: build.query<Product[], string | void>({
+      query: (search) => (
+        {
+          url:"/products", 
+          params: search ? {search}: {}
+        }),
+      providesTags: ["Products"],
+    }),
+    createProducts: build.mutation<Product, NewProduct>({
+      query: (newProduct) => ({
+        url:"/products",
+        method:"POST",
+        body: newProduct
+      }),
+      invalidatesTags:["Products"]
+    })
   }),
 });
 
-export const { useGetDashboardMetricsQuery } = api;
+export const { useGetDashboardMetricsQuery, useGetProductsQuery, useCreateProductsMutation } = api;
